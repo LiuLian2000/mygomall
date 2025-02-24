2fe5e53552e5254182bdbefb460fcbbb2b0de323
LiuLian2000 <LiuLian2000@users.noreply.github.com>
2025-02-24 17:42:27 +0800

    ✨ feat(初始化order代码，增加kitex鉴权中间键):

---
+++ pkg/mw/serverAuthMiddleware.go
@@ -0,0 +1,32 @@
+package mw
+
+import (
+	"context"
+
+	"github.com/Group-lifelong-youth-training/mygomall/pkg/errno"
+	"github.com/Group-lifelong-youth-training/mygomall/rpc_gen/kitex_gen/auth"
+	"github.com/bytedance/gopkg/cloud/metainfo"
+	"github.com/cloudwego/kitex/pkg/endpoint"
+)
+
+func AuthMiddleware(next endpoint.Endpoint) endpoint.Endpoint {
+	return func(ctx context.Context, request, response interface{}) error {
+		//获取 metadata
+		token, ok := metainfo.GetValue(ctx, "userId")
+
+		authResult, err := AuthClient.VerifyTokenByRPC(ctx, &auth.VerifyTokenReq{Token: token})
+
+		if err != nil {
+			return err
+		} else if authResult.BaseResp.StatusCode != errno.SuccessCode {
+			return errno.NewErrNo(int64(authResult.BaseResp.StatusCode), authResult.BaseResp.StatusMessage)
+		} else if authResult.Res != ok {
+			return errno.UnauthorizedDeliverRequestErr
+		}
+
+		// 6️⃣ 继续执行业务逻辑
+		err = next(ctx, request, response)
+
+		return err
+	}
+}
